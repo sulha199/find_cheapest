148b3580d4028785b73cc6e021e001a6a9e0943f
MITRAIS\Shulha_U3218 <sulha199@gmail.com>
2018-04-23 16:14:45 +0700

    clean the codes

--- src/App - helloworld.jsx
+++
@@ -1,5 +0,0 @@
-const contentNode = document.getElementById('contents');
-const continents = ['Africa','America','Asia','Australia','Europe'];
-const message = continents.map(c => `Hello ${c}!`).join({'<br>'}); 
-const component = <p>{message}</p>; // A simple JSX component
-ReactDOM.render(component, contentNode); // Render the component inside the content Node
