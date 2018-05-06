786a532888828112b773cfe98cf97c82b4668318
MITRAIS\Shulha_U3218 <sulha199@gmail.com>
2018-05-06 12:41:43 +0700

    config: config webpack to deploy in github

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 var webpack = require('webpack');
 var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 	//entry: './src/App.jsx',
@@ -12,7 +13,14 @@ module.exports = {
 		filename: 'app.bundle.js'
 	},
 	plugins: [
-		new webpack.optimize.CommonsChunkPlugin({name:'vendor',filename:'vendor.bundle.js'})
+		new webpack.optimize.CommonsChunkPlugin({name:'vendor',filename:'vendor.bundle.js'}),
+		new HtmlWebpackPlugin({
+            hash: true,
+            title: 'My Awesome application',
+            myPageHeader: 'Hello World',
+            template: './src/index.html',
+            //filename: './build/index.html' //relative to root of the application
+        })
 	],
 	module: {
 		loaders: [
